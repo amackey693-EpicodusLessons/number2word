4dcfec98a18464670e66f48ed01bc04c36aede0c
Mark Gridley <mark_gridley@yahoo.com>
2020-05-05 14:11:00 -0700

    write two more tests for teens and doubles, both tests PASS

--- lib/number2word.rb
+++ lib/number2word.rb
@@ -5,21 +5,16 @@ class NumWord
     @num = num
   end
 
-
-
   def singles()
     singles = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
   end
+
   def teens()
-    teens
-  end
-  def tens()
-    tens = {  }
-  end
-  def hundreds()
-    hundreds = {  }
+    teens = { 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
   end
 
-
-
+  def doubles()
+    doubles = { 10 => "ten", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
+  end
+  
 end
--- spec/number2word_spec.rb
+++ spec/number2word_spec.rb
@@ -1,9 +1,17 @@
 require('rspec')
 require('number2word')
 
-describe ("NumWord#singles") do 
+describe ("NumWord") do 
   it("returns the word for a number given 1-9") do 
     num1 = NumWord.new(1)
     expect(num1.singles.fetch(1)).to(eq("one"))
+  end
+  it("returns the word for a number given 11-19") do
+    num1 = NumWord.new(12)
+    expect(num1.teens.fetch(12)).to(eq("twelve"))
+  end
+  it("returns the word for a number given 10-90") do
+    num1 = NumWord.new(50)
+    expect(num1.doubles.fetch(50)).to(eq("fifty"))
   end
 end
