6e8478a78ab12d0db5297feb4ad1e38b728dde97
Mark Gridley <mark_gridley@yahoo.com>
2020-05-05 14:56:08 -0700

    write more tests, two are failing

--- lib/number2word.rb
+++ lib/number2word.rb
@@ -2,19 +2,31 @@
 require('pry')
 class NumWord
   def initialize(num)
-    @num = num
+    @num = num.to_s
   end
 
-  def singles()
-    singles = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
+  def create_word
+    word_num = @num.split('')
+    final_word = []
+    x = word_num[0]
+    y = word_num[1]
+
+    if word_num.length == 4 
+      final_word.push((singles.fetch(x) + " thousand ") + (singles.fetch(y) + " hundred"))
+    end
+    final_word 
   end
 
-  def teens()
-    teens = { 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
+  def singles
+    singles = { "0" => "", "1" => "one", "2" => "two", "3" => "three", "4" => "four", "5" => "five", "6" => "six", "7" => "seven", "8" => "eight", "9" => "nine" }
   end
 
-  def doubles()
-    doubles = { 10 => "ten", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
+  def teens
+    teens = { "11" => "eleven", "12" => "twelve", "13" => "thirteen", "14" => "fourteen", "15" => "fifteen", "16" => "sixteen", "17" => "seventeen", "18" => "eighteen", "19" => "nineteen" }
+  end
+
+  def doubles
+    doubles = { "10" => "ten", "20" => "twenty", "30" => "thirty", "40" => "forty", "50" => "fifty", "60" => "sixty", "70" => "seventy", "80" => "eighty", "90" => "ninety" }
   end
   
 end
--- spec/number2word_spec.rb
+++ spec/number2word_spec.rb
@@ -4,14 +4,22 @@ require('number2word')
 describe ("NumWord") do 
   it("returns the word for a number given 1-9") do 
     num1 = NumWord.new(1)
-    expect(num1.singles.fetch(1)).to(eq("one"))
+    expect(num1.singles.fetch("1")).to(eq("one"))
   end
   it("returns the word for a number given 11-19") do
     num1 = NumWord.new(12)
-    expect(num1.teens.fetch(12)).to(eq("twelve"))
+    expect(num1.teens.fetch("12")).to(eq("twelve"))
   end
   it("returns the word for a number given 10-90") do
     num1 = NumWord.new(50)
-    expect(num1.doubles.fetch(50)).to(eq("fifty"))
+    expect(num1.doubles.fetch("50")).to(eq("fifty"))
+  end
+  it("create_word method takes a number in the thousands and creates it to a word") do
+    num1 = NumWord.new(1000)
+    expect(num1.create_word).to(eq(["one thousand"]))
+  end
+  it("create_word method takes a number in the hundreds and creates it to a word") do
+    num1 = NumWord.new(1100)
+    expect(num1.create_word).to(eq(["one thousand one hundred"]))
   end
 end
